2c6a5d76cacc7ce089d1251e699955ad9e8cdef5
Laura Persilva <161455932+laurapersilva@users.noreply.github.com>
2025-03-21 09:15:34 -0300

    Update ContentView.swift

--- desafio02/desafio02/ContentView.swift
+++ desafio02/desafio02/ContentView.swift
@@ -1,24 +1,63 @@
 //
 //  ContentView.swift
-//  desafio02
+//  Desafio03
 //
-//  Created by Turma02-28 on 20/03/25.
+//  Created by Turma21-02 on 19/03/25.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var username = ""
+    @State private var popup = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("HackaFundo")
+                .resizable()
+                .frame(width: 900, height: 900)
+                .scaledToFill()
+                .opacity(0.1)
+            VStack () {
+                Text("Bem vindo, \(username)")
+                    .font(.largeTitle)
+                TextField("Digite seu nome", text: $username)
+                      .multilineTextAlignment(.center)
+            
+                Spacer()
+                
+                VStack {
+                    Image("HackaTransparente")
+                        .resizable()
+                        .frame(width: 230, height: 100)
+                        .scaledToFill()
+                    Image("HackaTransparente2")
+                        .resizable()
+                        .frame(width: 330, height: 90)
+                        .scaledToFill()
+                }
+                
+                Spacer()
+                
+                Button("Entrar") {
+                    popup = true
+                }
+                .alert(isPresented:$popup) {
+                    Alert(
+                        title: Text("ALERTA !"),
+                        message: Text("Você irá iniciar o desafio da aula agora"),
+                        dismissButton: .default(Text("Vamos lá!"))
+                    )
+                }
+            }
+            .scaledToFit()
+            .padding(EdgeInsets(top: 80, leading: 0, bottom: 60, trailing: 0))
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
