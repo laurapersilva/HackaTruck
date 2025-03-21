52c23cbf3e944e371af4d8face7b5b4a869dc6eb
Laura Persilva <161455932+laurapersilva@users.noreply.github.com>
2025-03-20 22:05:47 -0300

    Add files via upload

---
+++ desafio02/desafio02/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  desafio02
+//
+//  Created by Turma02-28 on 20/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
