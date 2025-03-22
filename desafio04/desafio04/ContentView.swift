ad0b6febab9276c6caad822bd3a56c7160c364b7
Laura Persilva <161455932+laurapersilva@users.noreply.github.com>
2025-03-21 21:34:52 -0300

    Add files via upload

---
+++ desafio04/desafio04/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  desafio04
+//
+//  Created by Turma02-28 on 21/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ desafio04/desafio04/desafio04App.swift
@@ -0,0 +1,17 @@
+//
+//  desafio04App.swift
+//  desafio04
+//
+//  Created by Turma02-28 on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct desafio04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
